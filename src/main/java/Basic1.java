c7eebe01270a3b2e4e7570eba5e8aefcfb812ac7
pyungpyang <thlee1229@gmail.com>
2022-08-10 23:07:37 +0900

    feat(init) : 프로젝트 세팅
    🔥 java 기본 타입

---
+++ src/main/java/Basic1.java
@@ -0,0 +1,43 @@
+public class Basic1 {
+
+    public void basic(){
+        System.out.println("Hello World");
+
+        int a1= 123;
+        int a2= -123;
+        System.out.println("a1 : " +a1);
+        System.out.println("a2 : " +a2);
+
+        long b1 = 123123;
+        long b2 = -123123;
+        System.out.println("b1 : " +b1);
+        System.out.println("b2 : " +b2);
+
+
+        char c1 = 'C';  // character
+        char[] cArray = new char[]{'a','b','c'};  // character
+        String c2 = "Chardadfdsfa";
+        System.out.println("c1 = " + c1);
+        System.out.println("cArray = " + cArray.toString());
+        System.out.println("c2 = " + c2);
+
+        int count = 10;
+        long grade = 123123;
+        System.out.println("plus = " +  (count + grade));
+        System.out.println("multiply = " + (count * grade));
+        System.out.println("div = " + (grade / count));
+        System.out.println("div = " + (grade % count));
+        System.out.println("div = " + (grade - count));
+
+
+//        while (true){
+//            System.out.println(" FUCK YOU");
+//        }
+
+//        for (int i = 0; i < 12; i++) {
+//            System.out.println(" FUCK YOU");
+//        }
+    }
+
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+        Basic1 basic1 = new Basic1();
+        basic1.basic();
+
+        // TODO: int long char -> primitive type
+
+        Long a = 12L;
+        Integer b = 12;
+        Character c = 'C';
+
+        System.out.println("a = " + a);
+        System.out.println(" Hello World.");
+        System.out.println(5 + 3);
+        System.out.println("Hello");
+        System.out.println("3+5=");
+        System.out.println(3 + 5);
+
+        final int MAX_VALUE;
+        MAX_VALUE = 100;
+        System.out.println("MAX_VALUE = " + MAX_VALUE);
+
+        String name = "Ja" + "Va";
+        System.out.println(name);
+        String str = name + 8.0;
+        System.out.println("name = " + str);
+
+        int x = 10;
+        int y = 20;
+        System.out.println(x + y);
+
+        int tmp = x;
+        x = y;
+        y = tmp;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+    }
+}
